527d91859ac8ceffec70db9d52df84afac857db4
4815908 <79232704+4815908@users.noreply.github.com>
2022-08-29 12:12:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "1e5289a4f9788c7086b5707368570a70",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "NPjoA_nlCOD_PkVlL6ZlS7qJx_pOUXkaPJn83tz1X6U",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["o2FQ566i8mHzzuvNhrtDT7qTv7O4","o2FQ566i8mHzzuvNhrtDT7qTv7O4"],
      
@@ -48,14 +48,14 @@ export const config = {
 
     }
 
-{{date.DATA}}  
- 城市：{{city.DATA}}  
- 天气：{{weather.DATA}}  
- 最低气温: {{min_temperature.DATA}}  
- 最高气温: {{max_temperature.DATA}}  
- 今天是我们恋爱的第{{love_day.DATA}}天
- 今天是我们结婚的第{{marry_day.DATA}}天
- {{birthday_message.DATA}}
+//{{date.DATA}}  
+ //城市：{{city.DATA}}  
+ //天气：{{weather.DATA}}  
+ //最低气温: {{min_temperature.DATA}}  
+// 最高气温: {{max_temperature.DATA}}  
+ //今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
+ //{{birthday_message.DATA}}
 
- {{note_en.DATA}}  
- {{note_ch.DATA}}
+// {{note_en.DATA}}  
+// {{note_ch.DATA}}
